e16b324a824290510ae267b01620f0b2bcb0d5b7
emikac <emikac@indiana.edu>
2018-02-26 12:55:28 -0500

    First commit for Tales From Covarnius Pilot

---
+++ Pilot/MusicHelper.swift
@@ -0,0 +1,9 @@
+//
+//  MusicHelper.swift
+//  Pilot
+//
+//  Created by Erik Mikac on 2/26/18.
+//  Copyright © 2018 Erik Mikac. All rights reserved.
+//
+
+import Foundation
--- Pilot/ViewController.swift
+++ Pilot/ViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class MainMenuViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createBeginButton()
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createBeginButton() {
+        let button = UIButton()
+        button.setTitle("Begin!", for: .normal)
+        button.setTitleColor(.cyan, for: .normal)
+        button.backgroundColor = .blue
+        button.frame = CGRect(x: view.frame.width/2 - 50, y: view.frame.height/1.25, width: 100, height: 36)
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 18
+        print(view.frame.height)
+        view.addSubview(button)
+        
     }
 
 
